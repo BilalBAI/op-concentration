12e21f1a0e8385960634bdbfa114e87985403aaf
yue.bai <bilal.yue.bai@gmail.com>
2024-10-08 16:25:49 -0400

    update dune uploader

--- core/dune_uploader.py
+++ core/dune_uploader.py
@@ -2,22 +2,28 @@ import dotenv
 import os
 from dune_client.client import DuneClient
 
-# change the current working directory where .env file lives
-os.chdir("/Users/bilalbai/Documents/Git/op-concentration/")
-# load .env file
-dotenv.load_dotenv(".env")
-# setup Dune Python client
-dune = DuneClient.from_env()
 
-# define path to your CSV file
-csv_file_path = 'data/test.csv'
+def upload_data():
+    # change the current working directory where .env file lives
+    os.chdir("./")
+    # load .env file
+    dotenv.load_dotenv(".env")
+    # setup Dune Python client
+    dune = DuneClient.from_env()
 
-with open(csv_file_path) as open_file:
-    data = open_file.read()
+    # define path to your CSV file
+    csv_file_path = 'data/test.csv'
 
-    table = dune.upload_csv(
-        data=str(data),
-        description="Measuring the Concentration of Power in the Collective",
-        table_name="concentration",
-        is_private=False
-    )
+    with open(csv_file_path) as open_file:
+        data = open_file.read()
+
+        table = dune.upload_csv(
+            data=str(data),
+            description="Measuring the Concentration of Power in the Collective",
+            table_name="concentration",
+            is_private=False
+        )
+
+
+if __name__ == "__main__":
+    upload_data()
